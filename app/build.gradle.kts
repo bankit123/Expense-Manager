b2260ffd295e3c207ca7c0abc5be4d2a8b26c4d5
bankit123 <bankitdhameliya702@gmail.com>
2025-11-20 22:55:19 +0530

    version 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "trackmyspend.budgetplanner.expensemanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.4"
+        versionCode = 5
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
